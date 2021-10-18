8bef88c605d60ea7f551d8fca1073c9e4454ccd9
rockomatthews <rob@robmatthe.ws>
2021-10-18 14:54:46 -0500

    Add AppleMIDI library and create simple momentary button that plays a note

--- MIDI5000Restart.ino
+++ MIDI5000Restart.ino
@@ -1,6 +1,28 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+
+#define SerialMon Serial
+#define APPLEMIDI_DEBUG SerialMon
+#include <AppleMIDI.h>
+
+char ssid[] = "ATT4K"; //  your network SSID (name)
+char pass[] = "passpasspass";    // your network password (use for WPA, or use as key for WEP)
+
 // set pin numbers
 const int buttonPin = 4;     // the number of the pushbutton pin
 const int ledPin =  16;      // the number of the LED pin
+
+unsigned long t0 = millis();
+int8_t isConnected = 0;
+
+APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+
+
 
 // variable for storing the pushbutton status 
 int buttonState = 0;
@@ -11,9 +33,50 @@ void setup() {
   pinMode(buttonPin, INPUT);
   // initialize the LED pin as an output
   pinMode(ledPin, OUTPUT);
+
+  DBG_SETUP(115200);
+  DBG("Booting");
+  
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DBG("Establishing connection to WiFi..");
+  }
+  DBG("Connected to network");
+
+  DBG(F("OK, now make sure you an rtpMIDI session that is Enabled"));
+  DBG(F("Add device named Arduino with Host"), WiFi.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
+  DBG(F("Select and then press the Connect button"));
+  DBG(F("Then open a MIDI listener and monitor incoming notes"));
+  DBG(F("Listen to incoming MIDI commands"));
+
+  MIDI.begin();
+
+  AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc, const char* name) {
+    isConnected++;
+    DBG(F("Connected to session"), ssrc, name);
+  });
+  AppleMIDI.setHandleDisconnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc) {
+    isConnected--;
+    DBG(F("Disconnected"), ssrc);
+  });
+  
+  MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOn"), note);
+  });
+  MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
+    DBG(F("NoteOff"), note);
+  });
+
+  DBG(F("Sending NoteOn/Off of note 45, every second"));
 }
 
 void loop() {
+
+  // Listen to incoming notes
+  MIDI.read();
+  
   // read the state of the pushbutton value
   buttonState = digitalRead(buttonPin);
   Serial.println(buttonState);
@@ -22,8 +85,10 @@ void loop() {
   if (buttonState == HIGH) {
     // turn LED on
     digitalWrite(ledPin, HIGH);
+    MIDI.sendNoteOn(53, 110, 13);
   } else {
     // turn LED off
     digitalWrite(ledPin, LOW);
+    MIDI.sendNoteOff(53, 110, 13);
   }
 }
