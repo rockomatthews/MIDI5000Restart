3bedf4f7b77e524a2b7cb18d339f8222ccd67568
rockomatthews <rob@robmatthe.ws>
2021-10-18 21:56:46 -0500

    Add momentary push button that plays note F2 when pressed

--- MIDI5000Restart.ino
+++ MIDI5000Restart.ino
@@ -10,11 +10,27 @@ char ssid[] = "ATT4K"; //  your network SSID (name)
 char pass[] = "passpasspass";    // your network password (use for WPA, or use as key for WEP)
 
 // set pin numbers
-const int buttonPin = 4;     // the number of the pushbutton pin
-const int ledPin =  16;      // the number of the LED pin
+const int button1_Pin = 4;     // the number of the pushbutton pin
+const int led1_Pin =  16;      // the number of the LED pin
 
+const int DEBOUNCE_DELAY = 50;   // the debounce time; increase if the output flickers
+
+// -----------------------------------------------------------------------------
+// button 1 states
+// -----------------------------------------------------------------------------
+int lastSteady1State = LOW;       // the previous steady state from the input pin
+int lastFlickerable1State = LOW;  // the previous flickerable state from the input pin
+int led1State = LOW;     // the current state of LED
+int lastButton1State;    // the previous state of button
+int currentButton1State; // the current state of button
+
+// -----------------------------------------------------------------------------
+// more variables
+// -----------------------------------------------------------------------------
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long t0 = millis();
 int8_t isConnected = 0;
+
 
 APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
 
@@ -23,16 +39,14 @@ APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
 // -----------------------------------------------------------------------------
 
 
-
-// variable for storing the pushbutton status 
-int buttonState = 0;
-
 void setup() {
   Serial.begin(115200);  
-  // initialize the pushbutton pin as an input
-  pinMode(buttonPin, INPUT);
-  // initialize the LED pin as an output
-  pinMode(ledPin, OUTPUT);
+  
+  pinMode(button1_Pin, INPUT_PULLUP); // set arduino pin to input pull-up mode
+  pinMode(led1_Pin, OUTPUT);          // set arduino pin to output mode
+
+  currentButton1State = digitalRead(button1_Pin);
+
 
   DBG_SETUP(115200);
   DBG("Booting");
@@ -61,15 +75,6 @@ void setup() {
     isConnected--;
     DBG(F("Disconnected"), ssrc);
   });
-  
-  MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity) {
-    DBG(F("NoteOn"), note);
-  });
-  MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity) {
-    DBG(F("NoteOff"), note);
-  });
-
-  DBG(F("Sending NoteOn/Off of note 45, every second"));
 }
 
 void loop() {
@@ -77,18 +82,32 @@ void loop() {
   // Listen to incoming notes
   MIDI.read();
   
-  // read the state of the pushbutton value
-  buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH
-  if (buttonState == HIGH) {
-    // turn LED on
-    digitalWrite(ledPin, HIGH);
+// -----------------------------------------------------------------------------
+// Button 1
+// -----------------------------------------------------------------------------
+   
+   if (currentButton1State != lastFlickerable1State) {
+      // reset the debouncing timer
+      lastDebounceTime = millis();
+      // save the the last flickerable state
+      lastFlickerable1State = currentButton1State;
+    }
+    
+    lastButton1State = currentButton1State;      // save the last state 
+    currentButton1State = digitalRead(button1_Pin);
+ 
+  if(lastButton1State == HIGH && currentButton1State == LOW) {
+    Serial.println("The button is pressed");
+    digitalRead(button1_Pin);
+    led1State = LOW;
+    digitalWrite(led1_Pin, led1State);
     MIDI.sendNoteOn(53, 110, 13);
-  } else {
-    // turn LED off
-    digitalWrite(ledPin, LOW);
-    MIDI.sendNoteOff(53, 110, 13);
-  }
+    } else if (lastButton1State == LOW && currentButton1State == HIGH) {
+      Serial.println("The button is released");
+      led1State = !led1State;
+      digitalWrite(led1_Pin, led1State);
+      MIDI.sendNoteOff(53, 0, 13);
+      // save the the last state
+      lastButton1State = currentButton1State;
+    }
 }
